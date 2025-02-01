61d2100f4a1b462ecc3b09dbad8fb26c8d0b1405
crossrepublic <71635944+ziminl@users.noreply.github.com>
2025-02-01 12:39:23 +0900

    Update auto screenlock.sh

--- auto screenlock.sh
+++ auto screenlock.sh
@@ -1,6 +1,27 @@
 #power manager -> 
-
 #system settings -> workspace behaviour -> screen locking
 
 loginctl unlock-sessions 
 systemctl unmask sleep.target
+
+sudo nano /etc/systemd/logind.conf
+# HandleSuspendKey=ignore 
+# HandleLidSwitch=ignore
+#IdleAction=ignore
+#IdleActionSec=0 
+sudo systemctl restart systemd-logind
+
+nano ~/.bashrc
+export TMOUT=0
+source ~/.bashrc
+
+sudo systemctl restart systemd-logind
+sudo reboot
+
+##ssh 
+#sudo nano /etc/ssh/sshd_config
+#ClientAliveInterval 0 
+#ClientAliveCountMax 0
+#sudo systemctl restart ssh 
+#sudo reboot
+
