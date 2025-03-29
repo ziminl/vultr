202530d8cd4de069b0c803928b39276813f2379d
crossrepublic <71635944+ziminl@users.noreply.github.com>
2025-03-29 10:31:11 +0900

    Update pip_alert.sh

--- pip_alert.sh
+++ pip_alert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-versions=$(ls -d /usr/lib/python*/ 2>/dev/null | sed 's|/usr/lib/python||;s|/||' | sort -V)
+versions=$(ls -d /usr/lib/python*/ 2>/dev/null | sed 's|/usr/lib/python||' | sort -V)
 
-for version in $versions; do
+echo "$versions" | while read -r version; do
     file="/usr/lib/python${version}/EXTERNALLY-MANAGED"
     if [[ -f "$file" ]]; then
         sudo rm -f "$file"
